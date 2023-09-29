fc51828c310ab39a66e56f7f8c3d7a9558a649b5
Hannah Ek <ekhannahek@gmail.com>
2023-09-29 14:14:03 +0200

    working on TextScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -29,6 +29,10 @@ const HomeScreen = ({ navigation }) => {
       onPress={() => navigation.navigate ('Square')}
         title="Go to square screen demo"
         />
+      <Button 
+      onPress={() => navigation.navigate ('Text')}
+        title="Go to text screen demo"
+        />
     </View>
   );
 };
